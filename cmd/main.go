30890b911690f62269c12d69f85381649f8c2314
André Saldanha <andre.g.saldanha@hotmail.com>
2024-08-15 22:56:13 -0300

    Initial Commit

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	db "new_ip_data_api/conn"
+	"new_ip_data_api/controller"
+	"new_ip_data_api/repository"
+	"new_ip_data_api/route"
+	"new_ip_data_api/usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	dbConnection, err := db.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	IpDataRepository := repository.NewIpDataRepository(dbConnection)
+	IpDataUsecase := usecase.NewIpDataUsecase(IpDataRepository)
+	ipDataController := controller.NewIpDataController(IpDataUsecase)
+	route.Endpoints(server, &ipDataController)
+}
